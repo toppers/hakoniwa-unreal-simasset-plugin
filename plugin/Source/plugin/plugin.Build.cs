24fc6323a1f6b4f66ff2ec26f3f38fae406e0ce7
Takashi Mori <kanetugu2015@gmail.com>
2024-01-16 16:04:37 +0900

    add external build option

--- plugin/Source/plugin/plugin.Build.cs
+++ plugin/Source/plugin/plugin.Build.cs
@@ -1,23 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class plugin : ModuleRules
 {
 	public plugin(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
+        
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        // ThirdParty/AssetsLibrary ディレクトリへのパス
+        string ThirdPartyPath = Path.Combine(ModuleDirectory, "../../../hakoniwa-core-cpp-client");
+        string LibCorePath = Path.Combine(ThirdPartyPath, "cmake-build/core/src");
+        string DyLibCorePath = Path.Combine(ThirdPartyPath, "cmake-build/src/assets");
+        string IncludePath = Path.Combine(ThirdPartyPath, "src/include");
+        
+        // ヘッダーファイルへのパスを追加
+        PublicIncludePaths.AddRange(new string[] { IncludePath });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        // 静的ライブラリへのリンクを追加
+        PublicAdditionalLibraries.Add(Path.Combine(LibCorePath, "libhako.a"));
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // 動的ライブラリへのリンクを追加
+        PublicDelayLoadDLLs.Add(Path.Combine(DyLibCorePath, "libassets.dylib"));
+        RuntimeDependencies.Add(Path.Combine(DyLibCorePath, "libassets.dylib"));
+        
+    }
 }
