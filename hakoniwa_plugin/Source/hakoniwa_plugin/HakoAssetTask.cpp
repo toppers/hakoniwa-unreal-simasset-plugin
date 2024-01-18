43e0dc9f7a87c88e6f56c595eab7698a94ed78c7
Takashi Mori <kanetugu2015@gmail.com>
2024-01-18 16:30:02 +0900

    fix #5

--- hakoniwa_plugin/Source/hakoniwa_plugin/HakoAssetTask.cpp
+++ hakoniwa_plugin/Source/hakoniwa_plugin/HakoAssetTask.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Misc/Paths.h"
+#include "Containers/StringConv.h"
 #include "HakoAssetTask.h"
 #include "hako_asset.h"
 #include <filesystem>
@@ -25,8 +26,19 @@ bool HakoAssetTask::Init()
     req_asset_simtime_usec = 0;
     current_simtime_usec = 0;
     //TODO absolute path to config settings.
-    const char* path = "/Users/tmori/project/oss/hakoniwa-unreal-simasset-plugin/hakoniwa_plugin/custom.json";
-    int ret = hako_asset_register("UnrealHakoAsset", path, &my_callback, HAKO_ASSET_DELTAT_TIME_USEC);
+    FString ProjectRoot = FPaths::ProjectDir();
+    // "/custom.json" を連結
+    FString FullPath = ProjectRoot + "custom.json";
+
+    // FString を std::string に変換
+    std::string StdString = TCHAR_TO_UTF8(*FullPath);
+
+    // std::string を const char* に変換
+    const char* CharPath = StdString.c_str();
+
+    //UE_LOG(LogTemp, Warning, TEXT("custom.json path: %s"), CharPath);
+    UE_LOG(LogTemp, Log, TEXT("hako_asset_register() success."));
+    int ret = hako_asset_register("UnrealHakoAsset", CharPath, &my_callback, HAKO_ASSET_DELTAT_TIME_USEC);
     if (ret != 0) {
         UE_LOG(LogTemp, Error, TEXT("hako_asset_register() error: ret = %d"), ret);
         return false;
